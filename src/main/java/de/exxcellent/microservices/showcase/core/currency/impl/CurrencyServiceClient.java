ceadce69f0290eb2464562841ea91bfb785f6de1
Felix Riess <felix.riess@exxcellent.de>
2020-01-22 10:42:48 +0100

    remove service url from application properties

--- src/main/java/de/exxcellent/microservices/showcase/core/currency/impl/CurrencyServiceClient.java
+++ src/main/java/de/exxcellent/microservices/showcase/core/currency/impl/CurrencyServiceClient.java
@@ -1,7 +1,6 @@
 package de.exxcellent.microservices.showcase.core.currency.impl;
 
 import de.exxcellent.microservices.showcase.core.currency.api.CurrencyESI;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  * A client to communicate with the currency service application. Implementation of {@link CurrencyESI}.
@@ -11,6 +10,5 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
  */
 public class CurrencyServiceClient implements CurrencyESI {
 
-    @ConfigProperty(name = "currency.service.base.url")
     private String currencyServiceBaseUrl;
 }
